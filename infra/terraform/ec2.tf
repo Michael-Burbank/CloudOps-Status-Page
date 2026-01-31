f3f79ea60abe55611e0b8e89393d9eba00f9f068
Michael Burbank <burbank2@yahoo.com>
2026-01-30 18:55:45 -0700

    Provision the AWS EC2 t3.small instance using Terraform. Terraform will find the official image on line 9, the defined instance on line 14, the root block device (EBS storage/IOPS/Throughput) on line 20, subnet and VPC configurations on line 27 w/ the key_name for SSH. Use the subnet for AZ 1a on line 42. Implement elastic IP (EIP) on line 55 while associating it with the EC2 instance.

---
+++ infra/terraform/ec2.tf
@@ -0,0 +1,63 @@
+# Define the provider.
+
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+}
+
+# Retrieve the latest Amazon Linux 2023 AMI with kernel 6.12.
+data "aws_ssm_parameter" "al2023_612" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-6.12-x86_64"
+}
+
+# Define the AWS EC2 instance.
+resource "aws_instance" "web_server" {
+  ami           = data.aws_ssm_parameter.al2023_612.value
+  instance_type = "t3.small"
+  monitoring    = true
+
+  # Configure the root block device with gp3 volume type, 10GB size, 3000 IOPS, and 125 MB/s throughput.
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = 10
+    iops        = 3000
+    throughput  = 125
+  }
+
+  # Add networking, key_name, and security groups as needed
+  subnet_id              = aws_subnet.main.id
+  vpc_security_group_ids = [var.security_group_id]
+  key_name               = var.aws_ec2_key_name
+}
+
+# Create the virtual private cloud (VPC).
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+# Create the subnet in the specified availability zone.
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = var.aws_availability_zone
+  tags = {
+    Name = "main-subnet"
+  }
+}
+
+resource "aws_eip" "web_eip" {
+  # No arguments needed for VPC EIP
+}
+
+# Create an Elastic IP and associate it with the EC2 instance.
+resource "aws_eip_association" "web_eip_assoc" {
+  instance_id   = aws_instance.web_server.id
+  allocation_id = aws_eip.web_eip.id
+}
+
+output "instance_public_ip" {
+  value = aws_eip.web_eip.public_ip
+}
